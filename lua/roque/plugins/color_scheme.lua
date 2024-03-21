082c1de24e2400660a57119c8abb395cebdd2609
roque-barbosa <roquejr1307@gmail.com>
2024-03-20 23:55:05 -0300

    add lazygit plugin

--- lua/roque/plugins/color_scheme.lua
+++ lua/roque/plugins/color_scheme.lua
@@ -1,46 +1,53 @@
 return {
-  {
-    "catppuccin/nvim",
-    name = "catppuccin",
-    config = function ()
-      require("catppuccin").setup({
-        flavour = "mocha", -- latte, frappe, macchiato, mocha
-        background = { -- :h background
-        dark = "mocha",
-      },
-      color_overrides = {
-        mocha = {
-          base = "#000000",
-          mantle = "#000000",
-          crust = "#000000",
-        },
-      },
-      integrations = {
-        telescope = {
-          enabled = true,
-        },
-        dropbar = {
-          enabled = true,
-          color_mode = true,
-        },
-      },
-    })
-    vim.cmd("colorscheme catppuccin")
-  end
-},
---[[
-{
-"morhetz/gruvbox",
-name = "gruvbox",
-config = function ()
-vim.cmd("colorscheme gruvbox")
-end
-},]]
---[[{
-  "Mofiqul/dracula.nvim",
-  name = "dracula",
-  config = function()
-  vim.cmd("colorscheme dracula")
+  --[[ {
+  "catppuccin/nvim",
+  name = "catppuccin",
+  config = function ()
+  require("catppuccin").setup({
+  flavour = "mocha", -- latte, frappe, macchiato, mocha
+  background = { -- :h background
+  dark = "mocha",
+  },
+  color_overrides = {
+  mocha = {
+  base = "#000000",
+  mantle = "#000000",
+  crust = "#000000",
+  },
+  },
+  integrations = {
+  telescope = {
+  enabled = true,
+  },
+  dropbar = {
+  enabled = true,
+  color_mode = true,
+  },
+  },
+  })
+  vim.cmd("colorscheme catppuccin")
   end
   },]]
+  --[[
+  {
+  "morhetz/gruvbox",
+  name = "gruvbox",
+  config = function ()
+  vim.cmd("colorscheme gruvbox")
+  end
+  },]]
+  {
+    "Mofiqul/dracula.nvim",
+    name = "dracula",
+    config = function()
+      require('dracula').setup({
+        colors = {
+          bg = '#000000',
+        },
+        italic_comment = true,
+        show_end_of_buffer = true,
+      })
+      vim.cmd("colorscheme dracula")
+    end
+  },
 }
--- lua/roque/plugins/lualine.lua
+++ lua/roque/plugins/lualine.lua
@@ -8,7 +8,7 @@ return {
         config = function()
                 require('lualine').setup({
                         options = {
-                                theme = 'catppuccin'
+                                theme = 'dracula'
                         }
                 }) 
         end      

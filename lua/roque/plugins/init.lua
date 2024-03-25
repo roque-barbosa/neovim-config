753c42cc2058c798dc4d1836a69e8e79349ff28d
roque-barbosa <roquejr1307@gmail.com>
2024-03-25 10:10:42 -0300

    feat: add colorizer and comment

--- lua/roque/plugins/init.lua
+++ lua/roque/plugins/init.lua
@@ -1,20 +1,32 @@
 return {
-        {
-                "nvim-tree/nvim-web-devicons",
-                name = "web-devicons",
-        },
-       	{
-		"nvim-lua/plenary.nvim",
-		name = "plenary"
-	},
-	{
-	        "nvim-treesitter/nvim-treesitter",
-	        build = ":TSUpdate",
-	},
-        {
-                'windwp/nvim-autopairs',
-                event = "InsertEnter",
-                config = true,
-        },
-        
+  {
+    "nvim-tree/nvim-web-devicons",
+    name = "web-devicons",
+  },
+  {
+    "nvim-lua/plenary.nvim",
+    name = "plenary"
+  },
+  {
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+  },
+  {
+    'windwp/nvim-autopairs',
+    event = "InsertEnter",
+    config = true,
+  },
+  {
+    'numToStr/Comment.nvim',
+    config = function()
+      require('Comment').setup()
+    end
+  },
+  {
+    'NvChad/nvim-colorizer.lua',
+    name = "colorizer",
+    config = function()
+      require('colorizer').setup()
+    end
+  },
 }

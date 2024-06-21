bfe25455b382ec0dfbfaa1cc5d43f0da36ea8e77
roque-barbosa <roquejr1307@gmail.com>
2024-06-20 22:48:44 -0300

    change theme and lualine

--- lua/roque/plugins/color_scheme.lua
+++ lua/roque/plugins/color_scheme.lua
@@ -43,9 +43,40 @@ return {
         end,
         -- Change the "hint" color to the "orange" color, and make the "error" color bright red
         on_colors = function (colors)
-         colors.bg = "#282a36"
-         colors.bg_dark = "#313442"
+         colors.bg = "#0C0C0C"
+         colors.bg_dark = "#222831"
          colors.comment = "#7a8099"
+         colors.green1 = "#B0EBB4"
+         colors.blue1 = "#7BD3EA"
+         colors.green = "#B5C18E"
+--[[ bg = "#24283b",
+  bg_highlight = "#292e42",
+  terminal_black = "#414868",
+  fg = "#c0caf5",
+  fg_dark = "#a9b1d6",
+  fg_gutter = "#3b4261",
+  dark3 = "#545c7e",
+  comment = "#565f89",
+  dark5 = "#737aa2",
+  blue0 = "#3d59a1",
+  blue = "#7aa2f7",
+  cyan = "#7dcfff",
+  blue1 = "#2ac3de",
+  blue2 = "#0db9d7",
+  blue5 = "#89ddff",
+  blue6 = "#b4f9f8",
+  blue7 = "#394b70",
+  magenta = "#bb9af7",
+  magenta2 = "#ff007c",
+  purple = "#9d7cd8",
+  orange = "#ff9e64",
+  yellow = "#e0af68",
+  green = "#9ece6a",
+  green1 = "#73daca",
+  green2 = "#41a6b5",
+  teal = "#1abc9c",
+  red = "#f7768e",
+  red1 = "#db4b4b", ]]
         end
       })
       vim.cmd("colorscheme tokyonight")
--- lua/roque/plugins/lualine.lua
+++ lua/roque/plugins/lualine.lua
@@ -4,6 +4,23 @@ return {
     'web-devicons',
   },
 
+  --[[ opts = function(_, opts)
+    local trouble = require("trouble")
+    local symbols = trouble.statusline({
+      mode = "lsp_document_symbols",
+      groups = {},
+      title = false,
+      filter = { range = true },
+      format = "{kind_icon}{symbol.name:Normal}",
+      -- The following line is needed to fix the background color
+      -- Set it to the lualine section you want to use
+      hl_group = "lualine_c_normal",
+    })
+    table.insert(opts.sections.lualine_c, {
+      symbols.get,
+      cond = symbols.has,
+    })
+  end, ]]
 
   config = function()
     require('lualine').setup({
@@ -30,6 +47,6 @@ return {
           }
         }
       }
-    }) 
-  end      
+    })
+  end
 }

d059925ba2e91494faff95161fc1b40df1785d39
roque-barbosa <roquejr1307@gmail.com>
2025-02-23 19:41:58 -0300

    linting

--- lua/roque/plugins/git.lua
+++ lua/roque/plugins/git.lua
@@ -1,10 +1,10 @@
 return {
-       "t ope/vim-fugitive",
-  {'akinsho/git-conflict.nvim', version = "*", config = true},
-  {
-    "lewis6991/gitsigns.nvim",
-    config = function()
-      require('gitsigns').setup()
-    end
-  }
+	"tpope/vim-fugitive",
+	{ "akinsho/git-conflict.nvim", version = "*", config = true },
+	{
+		"lewis6991/gitsigns.nvim",
+		config = function()
+			require("gitsigns").setup()
+		end,
+	},
 }
